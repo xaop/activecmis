57ed43d0c168c7515906684dbcabedb8b5a9e803
Joeri Samson <joeri@xaop.com>
2010-02-09 16:47:11 +0100

    Add HTTP Exceptions, fix inheritance
    Accidentally inherited from ::Exception instead of ActiveCMIS::Error,
    now fixed.
    
    Also adds 3 types of HTTPError, with a common superclass, but not a
    common superclass with the CMIS exception.
    The idea of these 3 exceptions is to abstract away from a possible use
    of Net::HTTP

--- lib/active_cmis/exceptions.rb
+++ lib/active_cmis/exceptions.rb
@@ -4,30 +4,30 @@ module ActiveCMIS
   class Error < StandardError
     # === Cause
     # One or more of the input parameters to the service method is missing or invalid
-    class InvalidArgument < Exception; end
+    class InvalidArgument < Error; end
 
     # === Cause
     # The service call has specified an object that does not exist in the Repository
-    class ObjectNotFound < Exception; end
+    class ObjectNotFound < Error; end
 
     # === Cause
     # The service method invoked requires an optional capability not supported by the repository
-    class NotSupported < Exception; end
+    class NotSupported < Error; end
 
     # === Cause
     # The caller of the service method does not have sufficient permissions to perform the operation
-    class PermissionDenied < Exception; end
+    class PermissionDenied < Error; end
 
     # === Cause
     # Any cause not expressible by another CMIS exception
-    class Runtime < Exception; end
+    class Runtime < Error; end
 
     # === Intent
     # The operation violates a Repository- or Object-level constraint defined in the CMIS domain model
     #
     # === Methods
     # see the CMIS specification
-    class Constraint < Exception; end
+    class Constraint < Error; end
     # === Intent
     # The operation attempts to set the content stream for a Document
     # that already has a content stream without explicitly specifying the
@@ -35,42 +35,48 @@ module ActiveCMIS
     #
     # === Methods
     # see the CMIS specification
-    class ContentAlreadyExists < Exception; end
+    class ContentAlreadyExists < Error; end
     # === Intent
     # The property filter or rendition filter input to the operation is not valid
     #
     # === Methods
     # see the CMIS specification
-    class FilterNotValid < Exception; end
+    class FilterNotValid < Error; end
     # === Intent
     # The repository is not able to store the object that the user is creating/updating due to a name constraint violation
     #
     # === Methods
     # see the CMIS specification
-    class NameConstraintViolation < Exception; end
+    class NameConstraintViolation < Error; end
     # === Intent
     # The repository is not able to store the object that the user is creating/updating due to an internal storage problam
     #
     # === Methods
     # see the CMIS specification
-    class Storage < Exception; end
+    class Storage < Error; end
     # === Intent
     #
     #
     # === Methods
     # see the CMIS specification
-    class StreamNotSupported < Exception; end
+    class StreamNotSupported < Error; end
     # === Intent
     #
     #
     # === Methods
     # see the CMIS specification
-    class UpdateConflict < Exception; end
+    class UpdateConflict < Error; end
     # === Intent
     #
     #
     # === Methods
     # see the CMIS specification
-    class Versioning < Exception; end
+    class Versioning < Error; end
+  end
+
+  class HTTPError < StandardError
+    class ServerError < HTTPError; end
+    class ClientError < HTTPError; end
+    class AuthenticationError < HTTPError; end
   end
 end
