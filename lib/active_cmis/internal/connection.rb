5ef48bb8b0aa22aeec3f5c32a00cf9afdb1eaace
Joeri Samson <joeri@xaop.com>
2010-02-22 16:00:09 +0100

    Add more HTTP methods to connection + user

--- lib/active_cmis/internal/connection.rb
+++ lib/active_cmis/internal/connection.rb
@@ -1,10 +1,12 @@
 module ActiveCMIS
   module Internal
     class Connection
+      attr_reader :user
       def authenticate(method, *params)
         case method
         when :basic
           @authentication = {:method => :basic_auth, :params => params}
+          @user = params.first
         else raise "Authentication method not supported"
         end
       end
@@ -12,20 +14,11 @@ module ActiveCMIS
       # The return value is the unparsed body, unless an error occured
       # If an error occurred, exceptions are thrown (see _ActiveCMIS::Exception
       def get(url)
-        case url
-        when URI; uri = url
-        else uri = URI.parse(url.to_s)
-        end
+        uri = normalize_url(url)
 
-        http = Net::HTTP.new(uri.host, uri.port)
         req = Net::HTTP::Get.new(uri.request_uri)
-        if uri.scheme == 'https'
-          http.use_ssl = true
-        end
-        if auth = @authentication
-          req.send(auth[:method], *auth[:params])
-        end
-        response = http.request req
+        http = authenticate_request(uri, req)
+        response = http.request(req)
 
         status = response.code.to_i
         if 200 <= status && status < 300
@@ -42,6 +35,48 @@ module ActiveCMIS
       def get_atom_entry(url)
         # FIXME: add validation that first child is really an entry
         get_xml(url).child
+      end
+
+      def put(url, body)
+        uri = normalize_url(url)
+        req = Net::HTTP::Put.new(uri.request_uri)
+        req.body = body
+        http = authenticate_request(uri, req)
+        response = http.request(req)
+      end
+
+      def post(url, body)
+        uri = normalize_url(url)
+        req = Net::HTTP::Post.new(uri.request_uri)
+        req.body = body
+        http = authenticate_request(uri, req)
+        response = http.request(req)
+      end
+
+      def delete(url)
+        uri = normalize_url(url)
+        req = Net::HTTP::Delete.new(uri.request_uri)
+        http = authenticate_request(uri, req)
+        response = http.request(req)
+      end
+
+      private
+      def normalize_url(url)
+        case url
+        when URI; url
+        else URI.parse(url.to_s)
+        end
+      end
+
+      def authenticate_request(uri, req)
+        http = Net::HTTP.new(uri.host, uri.port)
+        if uri.scheme == 'https'
+          http.use_ssl = true
+        end
+        if auth = @authentication
+          req.send(auth[:method], *auth[:params])
+        end
+        http
       end
     end
   end
