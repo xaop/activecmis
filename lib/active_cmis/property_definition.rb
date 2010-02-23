16dd52d10909f75ad825040882f2076384d4670d
Joeri Samson <joeri@xaop.com>
2010-02-23 14:08:27 +0100

    Don't throw exception for unset attributes
    Reason: opentext cmis compatibility
    The spec really says it shouldn't occur, but at the moment we just will
    have to live with it

--- lib/active_cmis/property_definition.rb
+++ lib/active_cmis/property_definition.rb
@@ -105,15 +105,18 @@ module ActiveCMIS
       end
       if elements.empty?
         if required
-          raise ActiveCMIS::Error.new("The server behaved strange: attribute #{self.inspect} required but not present among properties")
+          STDERR.puts "The server behaved strange: attribute #{self.inspect} required but no values specified"
+          # raise ActiveCMIS::Error.new("The server behaved strange: attribute #{self.inspect} required but not present among properties")
         end
         nil
       elsif elements.length == 1
         values = elements.first.children.select {|node| node.name == 'value' && node.namespace && node.namespace.href == ActiveCMIS::NS::CMIS_CORE}
         if required && values.empty?
-          raise ActiveCMIS::Error.new("The server behaved strange: attribute #{self.inspect} required but no values specified")
+          STDERR.puts "The server behaved strange: attribute #{self.inspect} required but no values specified"
+          # raise ActiveCMIS::Error.new("The server behaved strange: attribute #{self.inspect} required but no values specified")
         end
         if !repeating && values.length > 1
+          # STDERR.puts "The server behaved strange: attribute #{self.inspect} not repeating but multiple values given"
           raise ActiveCMIS::Error.new("The server behaved strange: attribute #{self.inspect} not repeating but multiple values given")
         end
         values
