c60bbf1de50b16e5f4763e5e7bf2ac6ea77ad2c0
Joeri Samson <joeri@xaop.com>
2010-03-09 14:06:05 +0100

    Expose child_collection_url
    Also fix case where there would be no down link (I'm not sure that this
    occurs, but either way it's better to return [] than nil)

--- lib/active_cmis/folder.rb
+++ lib/active_cmis/folder.rb
@@ -1,15 +1,24 @@
 module ActiveCMIS
   class Folder < ActiveCMIS::Object
     def items
-      query = "at:link[@rel = 'down' and @type = 'application/atom+xml;type=feed']/@href"
-      item_feed = data.xpath(query % 'feed', NS::COMBINED)
-      unless item_feed.empty?
-        feed = conn.get_xml(item_feed.to_s)
+      if url = child_collection_url
+        feed = conn.get_xml(url)
         feed.xpath('at:feed/at:entry', NS::COMBINED).map do |e|
           ActiveCMIS::Object.from_atom_entry(repository, e)
         end
+        # FIXME: handle next links
+      else
+        []
       end
     end
     cache :items
+
+    def child_collection_url
+      query = "at:link[@rel = 'down' and @type = 'application/atom+xml;type=feed']/@href"
+      item_feed = data.xpath(query, NS::COMBINED)
+      unless item_feed.empty?
+        item_feed.to_s
+      end
+    end
   end
 end
